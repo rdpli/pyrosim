eab4af16b8c3d5f6fe59e6644ef30d06f2f9b459
Sam Kriegman <sam.kriegman@uvm.edu>
2018-04-06 10:43:33 -0400

    shortq tests

---
+++ evodevo/submit_test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ -z "$1" ]
+then
+	echo "Number of runs not specified"
+	exit 1
+fi
+echo "Using number of TEST runs: " $1
+
+for x in `seq 1 $1`
+do
+	seed=${x}
+	qsub -vARG_SEED=${seed} ~/scratch/rigid_bodies/pyrosim/evodevo/single_runner_test.pbs
+	echo "TEST run $x started with seed $seed"
+done
