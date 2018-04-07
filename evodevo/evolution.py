d55ad7829d00c7696c7c916129da2a995eb8fa4e
Sam Kriegman <sam.kriegman@uvm.edu>
2018-04-07 11:10:40 -0400

    collisions on & larger mutations for canalization of larger legs

--- evodevo/evolution.py
+++ evodevo/evolution.py
@@ -4,7 +4,7 @@ import numpy as np
 from glob import glob
 from replicators import Population, Individual
 
-SEED = 7
+SEED = 1
 POP_SIZE = 30
 GENS = 4000
 
@@ -34,39 +34,36 @@ np.random.seed(SEED)
 # pop.save(DIR, SEED)
 
 
-# pickles = glob(DIR+'/*.p')
-#
-# run = pickles[SEED]
-#
-# with open(run, 'rb') as handle:
-#     pickle_dict = cPickle.load(handle)
-# print "got it"
-#
-# robot_ids = []
-# best_fit = 0
-# champ_idx = 0
-# for k, v in pickle_dict.items():
-#     robot_ids += [k]
-#     if v['fit'] > best_fit:
-#         champ_idx, best_fit = k, v['fit']
-#     # print run, best_fit
-#
-# rand_idx = np.random.choice(robot_ids)
-# champ_idx = rand_idx
+pickles = glob(DIR+'/*.p')
+
+run = pickles[SEED]
+
+with open(run, 'rb') as handle:
+    pickle_dict = cPickle.load(handle)
+print "got it"
+
+robot_ids = []
+best_fit = 0
+champ_idx = 0
+for k, v in pickle_dict.items():
+    robot_ids += [k]
+    if v['fit'] > best_fit:
+        champ_idx, best_fit = k, v['fit']
+    # print run, best_fit
 
 bot = Individual(0, 1)
-# bot.weight_matrix = pickle_dict[champ_idx]['weights']
-# bot.devo_matrix = pickle_dict[champ_idx]['devo']
+bot.weight_matrix = pickle_dict[champ_idx]['weights']
+bot.devo_matrix = pickle_dict[champ_idx]['devo']
 
 # bot.devo_matrix = np.ones_like(bot.devo_matrix)
 # bot.devo_matrix = np.zeros_like(bot.devo_matrix)
 
 print bot.devo_matrix
 
-# bot.turn_off_brain()
+bot.turn_off_brain()
 # bot.turn_off_body(1.0)
 print bot.calc_body_change(), bot.calc_control_change()
 
-bot.start_evaluation(seconds=SECONDS, dt=DT, blind=False, fancy=True, pause=True)
+bot.start_evaluation(seconds=SECONDS, dt=DT, blind=False, fancy=True, pause=True, debug=False)
 bot.compute_fitness()
 print bot.fitness
